f647fd84cfc8271825da23b966d950c11a1275ea
al-homse <adelresha.alking@gmail.com>
2026-09-01 02:00:59 +0300

    Fix Firebase initialization in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'views/home_screen.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  
+  // تهيئة Firebase في التطبيق
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+      apiKey: "AIzaSyCeTkG9y_J4f_eAw9f-s2mtfCWYbIAPK2w",
+      authDomain: "money-7eb69.firebaseapp.com",
+      projectId: "money-7eb69",
+      storageBucket: "money-7eb69.firebasestorage.app",
+      messagingSenderId: "609160171719",
+      appId: "1:609160171719:web:d0c37479729a30190a6ee2",
+    ),
+  );
+
   runApp(const ExpenseTrackerApp());
 }
 
